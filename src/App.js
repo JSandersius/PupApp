90fdc8bdb4ac932f1632334878d4fa4afb8c2ce7
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-15 15:16:10 -0700

    imported the three pages of this application, about,search and discover

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import Search from "./pages/Search"
+import Navabar from "./components/Navabar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 const App = () => 
   <Router>
