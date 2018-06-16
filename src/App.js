033e0a52f42c3df59bfa45943013071555641424
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-15 17:37:44 -0700

    fixing error

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Discover from "./pages/Discover";
 import About from "./pages/About";
 import Search from "./pages/Search"
-import Navabar from "./components/Navabar";
+import Navabar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 const App = () => 
   <Router>
     <div>
-      <Navar />
+      <Navbar />
       <Wrapper>
         <Route exact path="/" component={About} /> 
         <Route exact path="/about" component={About} />
