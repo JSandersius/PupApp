39a7f2b69f46a43504f6c7db4f543229ead23836
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-15 19:12:49 -0700

    moved pages to in within src foldet

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ const App = () =>
       <Wrapper>
         <Route exact path="/" component={About} /> 
         <Route exact path="/about" component={About} />
-        <Route exact path="/discover" component= {Discover} /> 
+        <Route exact path="/discover" component={Discover} /> 
         <Route exact path="/search" component={Search} />
       </Wrapper> 
       <Footer />
       </div>
   </Router>
-);
+// );
 
 export default App;
