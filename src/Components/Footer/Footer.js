0c0585faca8ab4e29296402e630ea8cb11a29454
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-15 18:18:41 -0700

    edited and finished Footer component file

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<footer classname="footer">
+		<span>Puppy App 2018</span>
+		</footer>
+		);
+
+export default Footer; 
