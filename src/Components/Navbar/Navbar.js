cae3e97e7135156e4853a2eb0e020bfd2ee21883
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-16 14:25:13 -0700

    error being thrown from index.js in src folder not allowing discover page from rendering

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 //Depending on the current path, this component sets the "active" calss on the appropriate navigation link item  
-
 const Navbar = props => <h1>Navbar</h1>
 
 export default Navbar; 
