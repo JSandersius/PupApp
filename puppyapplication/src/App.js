214f75451d99bc6db15c2e8524ef4ac066ac8cb6
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-19 16:01:50 -0700

    added imports to App.js

--- puppyapplication/src/App.js
+++ puppyapplication/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 import logo from './logo.svg';
 import './App.css';
 
