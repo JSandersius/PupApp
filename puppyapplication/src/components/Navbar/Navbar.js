6b867c0b2f56b3997bf1b0cb17ad23a230586037
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-19 15:25:23 -0700

    replaced src folder in new app

---
+++ puppyapplication/src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+//Depending on the current path, this component 
+//sets the "active" class on the appropriate navigation link item 
+const Navbar = props => <h1> Navbar </h1>
+
+export default Navbar;
