24f32a08f1c2241cdd0a59eaf9386355d34cfe45
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-19 01:02:26 -0700

    added react router and react router dom

--- puppyapp/src/components/Navbar/Navbar.js
+++ puppyapp/src/components/Navbar/Navbar.js
@@ -2,4 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-//Depending on the current path, this component sets the "active" class on the appropriate navigation link item 
+//Depending on the current path, this component 
+//sets the "active" class on the appropriate navigation link item 
+const Navbar = props => <h1> Navbar </h1>
+
+export default Navbar;
