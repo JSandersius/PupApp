c1fb2f71b13632e1823026569d29c1cb46b67b8c
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-19 13:37:59 -0700

    fixing errors

--- puppyapp/src/App.js
+++ puppyapp/src/App.js
@@ -2,17 +2,19 @@ import React  from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Discover from "./pages/Discover";
 import About from "./pages/About";
-import Search from "./pages/Search"
+import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
  
-const App = () => (
 //these components will only be rendered if the path is exactly matched
 //if we were to leave out the exact property, the component will render at the path starting
 //at the suggested path
 //if we don't add a path, we can chooese to render a component on no match whatsoever
 //this gives us the option to add a 404 page 
+
+const App = () => (
+
   <Router>
     <div>
         <Navbar />
