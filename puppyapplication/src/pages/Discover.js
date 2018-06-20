27e442cf3adac2383669e47b43ad2fc8ffc129ae
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-20 13:57:31 -0700

    built out discover component and notes

--- puppyapplication/src/pages/Discover.js
+++ puppyapplication/src/pages/Discover.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Discover = () => <h1>Discover</h1> 
+class Discover extends Component {
+    state = {
+        image: "",
+        matchCount: 0
+    };
 
-export default Discover; 
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Make New Friends</h1>
+                <h3 className="text-center">
+                    Thumbs up on any pups you'd like to meet!
+                    </h3>
+                <h1 className="text-center">
+                    Made friends with {this.state.matchCount} pups so far!
+                        </h1>
+            </div>
+
+        );
+    }
+}
+
+export default Discover;
 
