549ebcbc5762ef32f42b2fcca461ad57413d586b
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-19 18:06:06 -0700

    closing editor

--- puppyapplication/src/components/Navbar/Navbar.js
+++ puppyapplication/src/components/Navbar/Navbar.js
@@ -3,5 +3,19 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-const Navbar = props => <h1>Navbar</h1>
+const Navbar = props => 
+    <nav className="navbar navbar-default">
+        <div className="container-fluid">
+            <div classname="navbar-header">
+                <Link className="navbar-brand" to="/">
+                    Pupster
+                    </Link>
+                    </div>
+                    <ul> className="nav navbar-nav">
+                      <li> 
+                    
+                      
+
+
+
 export default Navbar;
