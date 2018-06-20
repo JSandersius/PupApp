2b49092a7967c48cf40cfc2de63809a2efd28ee2
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-20 14:40:20 -0700

    built out card component and spinner within

--- puppyapplication/src/components/Card/Card.js
+++ puppyapplication/src/components/Card/Card.js
@@ -1,26 +1,17 @@
 import React from "react";
-import CardBtn from "../CardBtn";
 import "./Card.css";
 
-const Card = props => (
-  <div
+const Card = props =>
+  <div>
     className="card"
     style={{
-      backgroundImage: props.image ? `url(${props.image})` : "none"
+      backgroundImage: props.image ? 'url(${props.image})' : "none"
     }}
-  >
-    {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
-    <CardBtn
-      style={{ opacity: props.image ? 1 : 0 }}
-      onClick={props.handleBtnClick}
-      data-value="pass"
-    />
-    <CardBtn
-      style={{ opacity: props.image ? 1 : 0 }}
-      onClick={props.handleBtnClick}
-      data-value="pick"
-    />
+    >
+    {!props.image && <i classname="fa fa-spinner fa-spin" aria-hidden="true" />}
   </div>
-);
+
+
+
 
 export default Card;
