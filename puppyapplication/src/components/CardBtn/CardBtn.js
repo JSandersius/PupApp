ff5d6d8452d98b9816e765362714f065cf2a4d07
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-20 22:53:27 -0700

    next is API call

--- puppyapplication/src/components/CardBtn/CardBtn.js
+++ puppyapplication/src/components/CardBtn/CardBtn.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./CardBtn.css";
 
-const CardBtn = props => (
+const CardBtn = props =>
   <button
     onClick={props.onClick}
     className={`card-btn ${props["data-value"]}`}
     {...props}
-  />
-);
+  />;
 
 export default CardBtn;
+
+//the jsx shortcut here means that the rest of the properties of this props object will be added
+//to the button element 
+//so in this case, all of the inline css will be applied as well as the date value 
