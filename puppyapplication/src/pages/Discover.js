60069dc28b0ee7ed9eee7f88da2cbfd8500ac034
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-22 15:54:16 -0700

    created new variable newState

--- puppyapplication/src/pages/Discover.js
+++ puppyapplication/src/pages/Discover.js
@@ -17,6 +17,25 @@ class Discover extends Component {
     handleBtnClick = event => {
         //Get the data value of the clicked button
         const btnType = event.target.attributes.getNamedItem("data-value").value;
+        //Clone this.state to the newState object
+        //We'll modify this object and use it to set our components state
+        const newState = { ...this.state };
+
+        if (btnType === "pick") {
+            //Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
+            newState.match = 1 === Match.floor(Math.random() * 5) + 1;
+
+            //Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us 
+            newState.matchCount = newState.match
+                ? newState.matchCount + 1
+                : newState.matchCount;
+        } else {
+            //if we thumbs down'ed the dog, we haven't matched with it 
+            newState.match = false;
+        }
+        //replace the component's state with newState, load the next image
+        this.setState(newState);
+        this.loadNextDog();
 
         console.log(btnType);
     };
