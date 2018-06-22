e40cb4ed3f486bf2d39f9af812d06817911cce97
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-22 15:20:33 -0700

    was missing ()'s in CardBtn.js

--- puppyapplication/src/components/CardBtn/CardBtn.js
+++ puppyapplication/src/components/CardBtn/CardBtn.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./CardBtn.css";
 
-const CardBtn = props =>
+const CardBtn = props => (
   <button
     onClick={props.onClick}
     className={`card-btn ${props["data-value"]}`}
     {...props}
-  />;
+  />
+);
 
 export default CardBtn;
 
