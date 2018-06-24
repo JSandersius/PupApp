822ede20420a9c835eb7f423a3441cbae3bf4eaa
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-24 15:30:13 -0700

    got yarn to start, edited API.js

--- puppyapplication/src/utils/API.js
+++ puppyapplication/src/utils/API.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-//Exports an object containing methods we'll use for accessing the Dog.Ceo API 
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
     getRandomDog: function () {
         return axios.get("https://dog.ceo/api/breeds/image/random");
     },
-
-    getBaseBreedList: function () {
+    getDogsOfBreed: function (breed) {
+        return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+    },
+    getBaseBreedsList: function () {
         return axios.get("https://dog.ceo/api/breeds/list");
     }
 };
-
 
 //we're going to export a method called get random dog which is going to make a GET request from the link provided in the instructions
