aefe815c434948e372261ba16e0fcbe5b2ce55c9
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-24 00:08:36 -0700

    building searchbar component

--- puppyapplication/src/components/SearchForm/SearchForm.js
+++ puppyapplication/src/components/SearchForm/SearchForm.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./SearchForm.css";
 
 const SearchForm = props =>
-    <form>
+    <form className="search">
+        <div className="form-group">
+            <label htmlFor="breed">Breed Name:</label>
+            <input
+                value={props.search}
+                onChange={props.handleInputChange}
+                name="breed"
+                type="text"
+                className="form-control;
+                placeholder="Type in a dog breed to begin"
+                id="breed"
+            />
+        </div>
+
     </form>
 
-export default Search 
+export default Search;
